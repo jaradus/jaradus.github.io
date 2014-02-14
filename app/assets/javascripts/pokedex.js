85ee2e7ee5cba1b0faa1c27a10f8313915950a45
Jaradus <ethanschmertzler@mac.com>
2014-02-14 17:43:08 -0500

    Update and Delete buttons put in, though they are now being taken off after click

--- app/assets/javascripts/pokedex.js
+++ app/assets/javascripts/pokedex.js
@@ -1,5 +1,11 @@
 var Pokemon = Backbone.Model.extend({
-
+    url: function(){
+      if(this.get("id")){
+        return "/pokemons/" + this.get("id")
+      } else {
+        return '/pokemons'
+      }
+    }
 });
 
 var PokemonCollection = Backbone.Collection.extend({
@@ -14,9 +20,46 @@ var PokemonView = Backbone.View.extend({
 
     template: function(attribute_hash) {
         var html_string = $("#pokemon_template").html();
+
         var template_function = _.template(html_string);
         return template_function(attribute_hash);
+    },
+
+    events: {
+      "click .update": "sync",
+      "click .delete": "delete"
+    },
+
+    sync: function(){
+      var self = this;
+      this.model.set({
+        attack:             this.$('.attack').val(),
+        catch_rate:         this.$('.catch_rate').val(),
+        defense:            this.$('.defense').val(),
+        egg_cycles:         this.$('.egg_cycles').val(),
+        ev_yield:           this.$('.ev_yield').val(),
+        exp:                this.$('.exp').val(),
+        growth_rate:        this.$('.growth_rate').val(),
+        happiness:          this.$('.happiness').val(),
+        height:             this.$('.height').val(),
+        hp:                 this.$('.hp').val(),
+        image_url:          this.$('.image_url').val(),
+        male_female_ratio:  this.$('.male_female_ratio').val(),
+        sp_atk:             this.$('.sp_atk').val(),
+        sp_def:             this.$('.sp_def').val(),
+        species:            this.$('.species').val(),
+        speed:              this.$('.speed').val(),
+        total:              this.$('.total').val(),
+        weight:             this.$('.weight').val()
+      })
+      this.model.save({}, {success: function(){ pokemon_list_view.collection.fetch()}})
+    },
+
+    delete: function(){
+      this.model.destroy()
     }
+
+
 });
 
 var PokemonListView = Backbone.View.extend({
@@ -43,9 +86,18 @@ var PokemonListView = Backbone.View.extend({
         _.each(this.collection.models, function(pokemon) {
             var pokemon_view = new PokemonView({model: pokemon });
             self.$el.append(pokemon_view.$el);
+
+            if (pokemon_view.model.get("id")) {
+                // Append edit and update buttons if item is in the database
+                pokemon_view.$('.pokemon_stats').append($("<button>", {class: "update", text: "update"}));
+                pokemon_view.$('.pokemon_stats').append($("<button>", {class: "delete", text: "delete"}));
+            }
+
             self.views.push(pokemon_view);
         });
-    }
+    },
+
+
 });
 
 
--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -11,24 +11,25 @@
    <td>
     </td>
       <td>
-        <ul>
-          <li>Attack: <%= attack %></li>
-          <li>Catch Rate: <%= catch_rate %></li>
-          <li>Defense: <%= defense %></li>
-          <li>Egg Cycles: <%= egg_cycles %></li>
-          <li>EV Yield: <%= ev_yield %></li>
-          <li>Exp: <%= exp %></li>
-          <li>Growth Rate: <%= growth_rate %></li>
-          <li>Happiness: <%= happiness %></li>
-          <li>Height: <%= height %></li>
-          <li>HP: <%= hp %></li>
-          <li>Male-to-Female Ratio: <%= male_female_ratio %></li>
-          <li>SP Attack: <%= sp_atk %></li>
-          <li>SP Defense: <%= sp_def %></li>
-          <li>Species: <%= species %></li>
-          <li>Speed: <%= speed %></li>
-          <li>Total: <%= total %></li>
-          <li>Weight: <%= weight %></li>
+        <ul class='pokemon_stats'>
+          <li>Attack:               <input type='text' class='attack_input' value=<%= attack %> /></li>
+          <li>Catch Rate:           <input type='text' class='catch_rate_input' value=<%= catch_rate %> /></li>
+          <li>Defense:              <input type='text' class='defense_input' value=<%= defense %> /></li>
+          <li>Egg Cycles:           <input type='text' class='egg_cycles_input' value=<%= egg_cycles %> /></li>
+          <li>EV Yield:             <input type='text' class='ev_yield_input' value=<%= ev_yield %> /></li>
+          <li>Exp:                  <input type='text' class='exp_input' value=<%= exp %> /></li>
+          <li>Growth Rate:          <input type='text' class='growth_rate_input' value=<%= growth_rate %> /></li>
+          <li>Happiness:            <input type='text' class='happiness_input' value=<%= happiness %> /></li>
+          <li>Height:               <input type='text' class='height_input' value=<%= height %> /></li>
+          <li>HP:                   <input type='text' class='hp_input' value=<%= hp %> /></li>
+          <li>Male-to-Female Ratio: <input type='text' class='male_female_ratio_input' value=<%= male_female_ratio %> /></li>
+          <li>SP Attack:            <input type='text' class='sp_atk_input' value=<%= sp_atk %> /></li>
+          <li>SP Defense:           <input type='text' class='sp_def_input' value=<%= sp_def %> /></li>
+          <li>Species:              <input type='text' class='species_input' value=<%= species %> /></li>
+          <li>Speed:                <input type='text' class='speed_input' value=<%= speed %> /></li>
+          <li>Total:                <input type='text' class='total_input' value=<%= total %> /></li>
+          <li>Weight:               <input type='text' class='weight_input' value=<%= weight %> /></li>
+          <li>Image Url:            <input type='text' class='image_url_input' value=<%= image_url %> /></li>
         </ul>
      </td>
  </tr>
