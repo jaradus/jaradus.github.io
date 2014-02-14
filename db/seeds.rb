6c52311539f1c6a2084997db9681c98865ed5696
Jaradus <ethanschmertzler@mac.com>
2014-02-14 15:46:52 -0500

    Seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "=============================="
+puts "Seed Task Started"
+puts "=============================="
+
+Pokemon.delete_all
+puts "All pokemon deleted"
+
+
+pokemon = HTTParty.get('http://pokeapi.co/api/v1/pokedex/1/')
+puts "Pokemon Names Downloaded"
+
+pokemon['pokemon'].each do |p|
+  pokemon_route = p['resource_uri']
+  data = HTTParty.get('http://pokeapi.co/'+pokemon_route)
+  image_url = 'http://http://pokeapi.co/media/img/'+data['national_id'].to_s+'.png'
+
+  new_pokemon = Pokemon.create({
+                  attack:             data['attack'],
+                  catch_rate:         data['catch_rate'],
+                  defense:            data['defense'],
+                  egg_cycles:         data['egg_cycles'],
+                  ev_yield:           data['ev_yield'],
+                  exp:                data['exp'],
+                  growth_rate:        data['growth_rate'],
+                  happiness:          data['happiness'],
+                  height:             data['height'],
+                  hp:                 data['hp'],
+                  image_url:          image_url,
+                  male_female_ratio:  data['male_female_ratio'],
+                  name:               data['name'],
+                  national_id:        data['national_id'],
+                  resource_uri:       data['resource_uri'],
+                  sp_atk:             data['sp_atk'],
+                  sp_def:             data['sp_def'],
+                  species:            data['species'],
+                  speed:              data['speed'],
+                  total:              data['total'],
+                  weight:             data['weight']
+                  })
+
+  puts "[Pokemon] #{new_pokemon.name} created"
+
+end
+
+puts "=============================="
+puts "Seed Task Completed"
+puts "=============================="
+
+
